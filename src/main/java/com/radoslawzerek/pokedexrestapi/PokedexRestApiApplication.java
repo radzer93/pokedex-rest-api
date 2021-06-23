b34389451a348d4b2276d416c56504ce4363dbbf
Radoslaw <zerek.r93@gmail.com>
2021-06-23 13:18:50 +0200

    Initial commit

---
+++ src/main/java/com/radoslawzerek/pokedexrestapi/PokedexRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.radoslawzerek.pokedexrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokedexRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/radoslawzerek/pokedexrestapi/PokedexRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.radoslawzerek.pokedexrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
